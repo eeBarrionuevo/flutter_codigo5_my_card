2109192c2cff7692fb3d89503c85384ba625e3b4
Mandarina <idcareg.g@gmail.com>
2022-04-21 22:09:37 -0500

    Modificación del Nombre

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MandarinaApp extends StatelessWidget {
               height: 10.0,
             ),
             Text(
-              "Fiorella Guadalupe",
+              "Fiorella de Fátima",
               style: TextStyle(
                   color: Colors.white,
                   fontSize: 22.0,
